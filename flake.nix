a2d9e13faf0a86bb697231abab9c199a9df130e7
GhostNoise <dev@ghostnoise.ru>
2026-01-30 18:17:30 +0300

    Renaming here and there

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 #
 # Let's quickly clear out what this is and isn't.
-# This is not a Nix package flake for Machine Brutality project (yet?).
+# This is not a Nix package flake for Artificial Threat project (yet?).
 # This IS a flake for the "nix develop" command.
 #
 {
-  description = "Samucard Development Environment";
+  description = "Artificial Threat development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -17,12 +17,12 @@
     devShells.${system} = {
       default = pkgs.mkShell {
         packages = with pkgs; [
-	  gcc
-	  gnumake
-	  libopenmpt
-	  allegro	# Yup, it's 4.4 and not 4.2, so it would have (some) differences compared to a DOS-compatible Allegro version.
+          gcc
+          gnumake
+          libopenmpt
+          allegro	# Yup, it's 4.4 and not 4.2, so it would have (some) differences compared to a DOS-compatible Allegro version.
           lua
-	];
+        ];
       };
     };
   };
